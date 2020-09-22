08847ec6a3bf33553f3f5049521326e8f87d5920
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2020-09-21 21:17:44 -0300

    Updates

---
+++ precache-manifest.4c06de473fdfb0dc2ef97ea1f9224c20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a49a72312113652d5147ab01c6117d4",
+    "url": "/07-heroes-app/index.html"
+  },
+  {
+    "revision": "e48dbc9a764fb25c969d",
+    "url": "/07-heroes-app/static/js/2.90f62444.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/07-heroes-app/static/js/2.90f62444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e014affbb38487a4a05",
+    "url": "/07-heroes-app/static/js/main.5670d3e4.chunk.js"
+  },
+  {
+    "revision": "5a9feffdbd0aa5ff997b",
+    "url": "/07-heroes-app/static/js/runtime-main.11d2d58e.js"
+  }
+]);
