36c2ab022cbd6cf1b6a38c1b47b61c6ac11b47df
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2020-09-23 21:53:39 -0300

    Updates

---
+++ precache-manifest.abeb3ec98dca60bc06523b43d6188864.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae430f940b44dc961f9014f1c631716d",
+    "url": "/07-heroes-app/index.html"
+  },
+  {
+    "revision": "0e02cd612424451b4142",
+    "url": "/07-heroes-app/static/js/2.ecb1c437.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/07-heroes-app/static/js/2.ecb1c437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc715e93fbe78d94153",
+    "url": "/07-heroes-app/static/js/main.22733b08.chunk.js"
+  },
+  {
+    "revision": "5a9feffdbd0aa5ff997b",
+    "url": "/07-heroes-app/static/js/runtime-main.11d2d58e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/07-heroes-app/precache-manifest.d36583fcadc2f99d8ab7ad4b0c1a93c2.js"
+  "/07-heroes-app/precache-manifest.abeb3ec98dca60bc06523b43d6188864.js"
 );
 
 self.addEventListener('message', (event) => {
